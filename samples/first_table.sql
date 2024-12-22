56921e993e448cf29331b9d07ea70ff3740435c2
Yung-Chung Ku <ycku@csie.org>
2024-12-22 18:25:51 +0800

    Create first_table.sql

---
+++ samples/first_table.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE finops OWNER user99;
+
+CREATE SCHEMA user99;
+
+SET SEARCH_PATH=user99;
+
+CREATE TABLE expenses (
+	uuid uuid DEFAULT gen_random_uuid() NOT NULL,
+	trading_date date DEFAULT now() NULL,
+	category text NULL,
+	value int4 NULL,
+	note text NULL,
+	CONSTRAINT expenses_pk PRIMARY KEY (uuid)
+);
